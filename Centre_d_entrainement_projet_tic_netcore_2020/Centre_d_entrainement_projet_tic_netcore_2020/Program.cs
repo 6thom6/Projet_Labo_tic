bd07d317b6582c9385698dbeabf440aba26431e7
6thom6 <66666050+6thom6@users.noreply.github.com>
2020-08-20 14:25:16 +0200

    creation_db

---
+++ Centre_d_entrainement_projet_tic_netcore_2020/Centre_d_entrainement_projet_tic_netcore_2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Centre_d_entrainement_projet_tic_netcore_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
